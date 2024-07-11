a7d44ac1ae108a7f3d7622fd3d379f888eaaf217
kiirona20 <allankiironen7@gmail.com>
2024-07-11 17:05:04 +0300

    tehtävä 3.20*

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const errorHandler = (error, requst, response, next) => {
 
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
   
     next(error)
@@ -90,20 +92,9 @@ app.delete('/api/people/:id', (request, response, next) => {
 })
 
 
-app.post('/api/people', (request, response) => {
+app.post('/api/people', (request, response, next) => {
     const body = request.body
-    const duplicateName = people.find(person => person.name === body.name)
 
-    if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'name or number missing' 
-      })
-    }
-    else if (duplicateName){
-        return response.status(400).json({ 
-            error: 'name must be unique'  
-          })
-    }
     const person = new Person({
       name: body.name,
       number: body.number
@@ -111,20 +102,17 @@ app.post('/api/people', (request, response) => {
   
     person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
+  }).catch(error => next(error))
     morgan.token('type', function (request, response) { return req.headers['content-type'] })
 
 })
 
 app.put('/api/people/:id', (request, response, next) => {
-    const body = request.body
-    console.log("body")
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+    const { name, number} = request.body
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true})
+    Person.findByIdAndUpdate(request.params.id, 
+    { name, number},
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
         response.json(updatedNote)
     })
--- models/person.js
+++ models/person.js
@@ -14,9 +14,26 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-/.test(v);
+      },
+      validator: function(v) {
+        return /\d{2}-/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
